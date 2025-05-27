4d4ac49889bf1d1a8a50f06a0ce5c608dfe65b95
Guille Ferchero <guilleferchero@gmail.com>
2025-05-27 18:57:23 -0300

    normalizo nombres

--- grafico_tiros_vs_posesion_liga_argentina.R
+++ grafico_tiros_vs_posesion_liga_argentina.R
@@ -1,20 +1,16 @@
 rm(list = ls())
 
 # Librerías necesarias
-#install.packages(c("worldfootballR", "dplyr", "ggplot2", "ggimage", "stringr"))
+
 library(worldfootballR)
 library(dplyr)
 library(ggplot2)
 library(ggimage)
 library(stringr)
 
-# install.packages("devtools")
-# devtools::install_github("JaseZiv/worldfootballR", force = TRUE)
-# library(worldfootballR)
 
-
-# Descargar estadísticas de tiros y posesión
-shots <- fb_season_team_stats(
+# Descargar estadísticas
+tiros <- fb_season_team_stats(
   country = "ARG",
   gender = "M",
   season_end_year = 2025,
@@ -22,7 +18,7 @@ shots <- fb_season_team_stats(
   stat_type = "shooting"
 )
 
-poss <- fb_season_team_stats(
+posesion <- fb_season_team_stats(
   country = "ARG",
   gender = "M",
   season_end_year = 2025,
@@ -39,50 +35,29 @@ stand <- fb_season_team_stats(
 )
 
 
+#Limpieza y construccion del dataset
+
 stats <- stand %>% 
   select(Squad, Poss, Gls, xG_Expected) %>% 
   slice(1:30)
 
 stats_aux <- stand %>% 
-  select(Squad, xG_Expected) %>%
-  rename(xGA = xG_Expected) %>% 
+  select(Squad, xG_Expected,Gls) %>%
+  rename(xGA = xG_Expected,
+         GlsA = Gls) %>% 
   slice(31:60) %>% 
   mutate(Squad = gsub("^\\s*[Vv][Ss]?\\s*", "", Squad))
 
 stats_def <- stats %>% 
   left_join(stats_aux)
 
-# # Combinar estadísticas
-# arg_stats <- shots %>%
-#   select(Squad, Shots = `Sh_Standard`) %>%
-#   inner_join(
-#     poss %>% select(Squad, Poss = `Poss`),
-#     by = "Squad"
-#   ) %>%
-#   mutate(Poss = as.numeric(str_remove(Poss, "%"))) %>% 
-#   slice(1:30) %>% #selecciono las primeras 30 
-#   mutate(
-#       Equipo = str_to_lower(Squad)) %>% 
-#   select(!c(Squad)) %>% 
-#   relocate(Equipo)
-
-
-
-# # Unir datos con escudos
-# arg_plot_data <- arg_stats %>%
-#   inner_join(logos, by = "Squad")
-
+################################Escudos#############################################################################
 # Lista archivos con sus rutas
 archivos <- list.files("escudos/", pattern = "\\.png$", full.names = TRUE)
 nombres <- tools::file_path_sans_ext(basename(archivos))
 
 # Crear un data frame con nombres y rutas
 df_imagenes <- data.frame(equipo = nombres, ruta_imagen = archivos, stringsAsFactors = FALSE)
-
-# Unir con tu data frame original
-df <- merge(df, df_imagenes, by = "equipo", all.x = TRUE)
-
-# FALTA IGUALAR LOS NOMBRES
 
 df_imagenes <- df_imagenes %>% 
   mutate(nombre_equipo = case_when(equipo == "aldosivi" ~ "Aldosivi",
@@ -118,14 +93,19 @@ df_imagenes <- df_imagenes %>%
   select(nombre_equipo,ruta_imagen) %>% 
   rename(Squad = nombre_equipo)
 
+#############################################################################################################
 
 
+#Le agrego las img al df
 stats <- stats_def %>% 
   left_join(df_imagenes)
 
+# Grabamos el df final
 write.csv(stats, "data/stats.csv")
 
-# Crear gráfico
+
+##Gráficos###
+
 xG_vs_xGA <- ggplot(stats, aes(x = xG_Expected, y = xGA)) +
   geom_image(aes(image = ruta_imagen), size = 0.07) +
   geom_vline(xintercept = mean(stats$xG_Expected), linetype = "dashed", color = "grey") +
@@ -138,3 +118,18 @@ xG_vs_xGA <- ggplot(stats, aes(x = xG_Expected, y = xGA)) +
   theme_minimal()
 
 ggsave("xG_vs_xGA.png", xG_vs_xGA)
+
+
+G_vs_GA <- ggplot(stats, aes(x = Gls, y = GlsA)) +
+  geom_image(aes(image = ruta_imagen), size = 0.07) +
+  geom_vline(xintercept = mean(stats$Gls), linetype = "dashed", color = "grey") +
+  geom_hline(yintercept = mean(stats$GlsA), linetype = "dashed", color = "grey") +
+  labs(
+    title = "Goles a Favor vs Goles en Contra – Liga Argentina 2023/24",
+    x = "Goles a Favor",
+    y = "Goles en Contra"
+  ) +
+  theme_minimal()
+
+ggsave("G_vs_GA.png", G_vs_GA)
+
